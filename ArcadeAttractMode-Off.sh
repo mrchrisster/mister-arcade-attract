cf3f5fd87138789e1b610806526ccdd410b58e84
mrchrisster <60834906+mrchrisster@users.noreply.github.com>
2021-01-27 15:57:31 -0800

    Create ArcadeAttractMode-Off.sh

---
+++ ArcadeAttractMode-Off.sh
@@ -0,0 +1,14 @@
+mount | grep -q "on / .*[(,]ro[,$]" && RO_ROOT="true"
+[ "$RO_ROOT" == "true" ] && mount / -o remount,rw
+mv /etc/init.d/S98attract /etc/init.d/_S98attract > /dev/null 2>&1
+sync
+[ "$RO_ROOT" == "true" ] && mount / -o remount,ro
+sed -i "s/.*bootcore=.*/#bootcore=/" /media/fat/MiSTer.ini
+
+echo "Attract mode is off and"
+echo "inactive at startup."
+echo "Done!"
+echo "Rebooting in 5s"
+sync
+sleep 5 && fpga /media/fat/menu.rbf
+exit 0
