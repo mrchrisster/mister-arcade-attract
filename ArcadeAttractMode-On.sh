1c6fad33604a0a165fc7efb6bd0858eb781038e9
mrchrisster <60834906+mrchrisster@users.noreply.github.com>
2021-01-27 15:57:04 -0800

    Create ArcadeAttractMode-On.sh

---
+++ ArcadeAttractMode-On.sh
@@ -0,0 +1,55 @@
+#This cycles through Arcade cores every 5 mins 
+
+mount | grep "on / .*[(,]ro[,$]" -q && RO_ROOT="true"
+[ "$RO_ROOT" == "true" ] && mount / -o remount,rw
+cat <<\EOF > /etc/init.d/_S98attract
+#!/bin/sh
+trap "" HUP
+trap "" TERM
+sleepfpga()
+{
+    sleep 300
+    mra=$(shuf -n 1 /media/fat/Scripts/mra.txt)
+    sed -i "s/.*bootcore=.*/bootcore=$mra/" /media/fat/MiSTer.ini
+    fpga /media/fat/menu.rbf
+}
+start() {
+        printf "Starting Attract Mode: "
+        sleepfpga &
+        echo $!>/var/run/attract.pid
+}
+stop() {
+        printf "Stopping Attract Mode: "
+        kill -9 `attract.pid`
+        rm /var/run/attract.pid
+        echo "OK"
+}
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    restart)
+        stop
+        start
+        ;;
+    *)
+        echo "Usage: /etc/init.d/S98attract {start|stop|restart}"
+        exit 1
+        ;;
+esac
+exit 0
+EOF
+
+
+mv /etc/init.d/_S98attract /etc/init.d/S98attract > /dev/null 2>&1
+chmod +x /etc/init.d/S98attract
+sync
+[ "$RO_ROOT" == "true" ] && mount / -o remount,ro
+sync
+/etc/init.d/S98attract start
+    mra=$(shuf -n 1 /media/fat/Scripts/mra.txt)
+    sed -i "s/.*bootcore=.*/bootcore=$mra/" /media/fat/MiSTer.ini
+    fpga /media/fat/menu.rbf
