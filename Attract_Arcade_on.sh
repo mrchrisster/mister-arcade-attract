f92842ffc68f6d3254298ab54e02fa4fa6f187ea
Mellified <81110968+Mellified@users.noreply.github.com>
2021-03-25 01:39:24 -0500

    Add files via upload
    * Changed default time to 120 seconds
    * Added loop when direct loading cores
    * Added logic to reboot after loading 3 cores to prevent corruption issues
    * Daemon start now checks for existing PID and kills it to prevent multiple instances - assumption is something went wrong
    * Confirm mra is a file before trying to load it
    * Renamed scripts to Attract_Arcade_on.sh and Attract_Arcade_off.sh to sort with other Attract scripts in Scripts menu
    * Renamed mra.txt to Attract_Arcade.txt for consistency with new file names
    * Added Attract_Arcade.txt updater script - simply dumps *.mra from _Arcade directory
    * Anal syntax tweaks

--- Attract_Arcade_on.sh
+++ Attract_Arcade_on.sh
@@ -1,28 +1,53 @@
-#This cycles through Arcade cores every 5 mins 
+# This cycles through Arcade cores listed in mra.txt periodically
 
 mount | grep "on / .*[(,]ro[,$]" -q && RO_ROOT="true"
-[ "$RO_ROOT" == "true" ] && mount / -o remount,rw
+[ "${RO_ROOT}" == "true" ] && mount / -o remount,rw
 cat <<\EOF > /etc/init.d/_S98attract
 #!/bin/sh
 trap "" HUP
 trap "" TERM
+
 sleepfpga()
 {
-    sleep 300
-    mra=$(shuf -n 1 /media/fat/Scripts/mra.txt)
-    echo "load_core /media/fat/_Arcade/$mra" > /dev/MiSTer_cmd
+    i=3
+    while (( i > 0 )); do
+    	sleep 120
+    	mra=$(shuf -n 1 /media/fat/Scripts/Attract_Arcade.txt)
+    	until [ -f "/media/fat/_Arcade/${mra}" ]; do
+    		mra=$(shuf -n 1 /media/fat/Scripts/Attract_Arcade.txt)
+    	done
+    	echo "load_core /media/fat/_Arcade/${mra}" > /dev/MiSTer_cmd
+    	((i++))
+    done
+    
+    sleep 120
+    mra=$(shuf -n 1 /media/fat/Scripts/Attract_Arcade.txt)
+    until [ -f "/media/fat/_Arcade/${mra}" ]; do
+    	mra=$(shuf -n 1 /media/fat/Scripts/Attract_Arcade.txt)
+   	done
+    sed -i "s/.*bootcore=.*/bootcore=${mra}/" /media/fat/MiSTer.ini
+    fpga /media/fat/menu.rbf
+
+    sleepfpga &
 }
+
 start() {
+				if [ -f /var/run/attract.pid ]; then
+					kill -9 $(cat /var/run/attract.pid)
+					rm -f /var/run/attract.pid
+				fi
         printf "Starting Attract Mode: "
         sleepfpga &
         echo $!>/var/run/attract.pid
 }
+
 stop() {
         printf "Stopping Attract Mode: "
         kill -9 `attract.pid`
-        rm /var/run/attract.pid
+        rm -f /var/run/attract.pid
         echo "OK"
 }
+
 case "$1" in
     start)
         start
@@ -39,6 +64,7 @@ case "$1" in
         exit 1
         ;;
 esac
+
 exit 0
 EOF
 
@@ -46,8 +72,11 @@ EOF
 mv /etc/init.d/_S98attract /etc/init.d/S98attract > /dev/null 2>&1
 chmod +x /etc/init.d/S98attract
 sync
-[ "$RO_ROOT" == "true" ] && mount / -o remount,ro
+[ "${RO_ROOT}" == "true" ] && mount / -o remount,ro
 sync
 /etc/init.d/S98attract start
-    mra=$(shuf -n 1 /media/fat/Scripts/mra.txt)
-    echo "load_core /media/fat/_Arcade/$mra" > /dev/MiSTer_cmd
+    mra=$(shuf -n 1 /media/fat/Scripts/Attract_Arcade.txt)
+		until [ -f "/media/fat/_Arcade/${mra}" ]; do
+			mra=$(shuf -n 1 /media/fat/Scripts/Attract_Arcade.txt)
+		done
+    echo "load_core /media/fat/_Arcade/${mra}" > /dev/MiSTer_cmd
