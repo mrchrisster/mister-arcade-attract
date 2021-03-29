1b0622bbb0c98ef3b6e44d75f930adde5960d91d
Mellified <81110968+Mellified@users.noreply.github.com>
2021-03-29 16:45:18 -0500

    No longer needed

--- Attract_Arcade_All_update.sh
+++
@@ -1,3 +0,0 @@
-echo "Updating /media/fat/Scripts/Attract_Arcade_All.txt"
-ls -N1 /media/fat/_Arcade/*.mra | sed 's/\/media\/fat\/_Arcade\///' | tee /media/fat/Scripts/Attract_Arcade_All.txt
-echo "Update complete!"
