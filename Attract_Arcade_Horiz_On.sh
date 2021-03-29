7254284e9751eef00538d846d0372eac88da1f80
mrchrisster <60834906+mrchrisster@users.noreply.github.com>
2021-03-29 13:10:44 -0700

    Update Attract_Arcade_Horiz_On.sh

--- Attract_Arcade_Horiz_On.sh
+++ Attract_Arcade_Horiz_On.sh
@@ -11,10 +11,7 @@
 echo "Welcome to Attract mode for MiSTer arcade games!"
 echo ""
 echo "To disable:"
-echo "1. Open the OSD (F12) and go right"
-echo "2. Select reboot"
-echo "3. Open the OSD again and open the Scripts menu"
-echo "4. Select attract_arcade_off"
+echo "Reboot the MiSTer"
 echo ""
 echo "Starting in..."
 for i in {5..1}; do
@@ -22,14 +19,6 @@ for i in {5..1}; do
 	sleep 1
 done
 
-
-# Create the daemon script on disk
-mount | grep "on / .*[(,]ro[,$]" -q && RO_ROOT="true"
-[ "${RO_ROOT}" == "true" ] && mount / -o remount,rw
-cat <<\EOF > /etc/init.d/_S98attract
-#!/bin/sh
-trap "" HUP
-trap "" TERM
 
 nextcore()
 {
@@ -52,58 +41,6 @@ sleepfpga()
   done
 }
 
-start() {
-	# If another attract process is running kill it
-	# This can happen if the MiSTer is rebooted manually
-	if [ -f /var/run/attract.pid ]; then
-		kill -9 $(cat /var/run/attract.pid)
-	fi
-  printf "Starting Attract Mode: "
-  sleepfpga &
-  echo $!>/var/run/attract.pid
-}
-
-stop() {
-  printf "Stopping Attract Mode: "
-	if [ -f /var/run/attract.pid ]; then
-		kill -9 $(cat /var/run/attract.pid)
-    rm -f /var/run/attract.pid
-	fi
-  echo "OK"
-}
-
-case "$1" in
-    start)
-        start
-        ;;
-    stop)
-        stop
-        ;;
-    restart)
-        stop
-        start
-        ;;
-		next)
-				nextcore
-				;;
-    *)
-        echo "Usage: /etc/init.d/S98attract {start|stop|restart|next}"
-        echo "Note: next will load the next core immediately but does not reset the timer!"
-        exit 1
-        ;;
-esac
-
-exit 0
-EOF
 
 
-# Install the daemon
-mv /etc/init.d/_S98attract /etc/init.d/S98attract > /dev/null 2>&1
-chmod +x /etc/init.d/S98attract
-sync
-[ "${RO_ROOT}" == "true" ] && mount / -o remount,ro
-sync
-
-
-# Run the daemon
-/etc/init.d/S98attract start
+sleepfpga
