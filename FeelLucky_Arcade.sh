5c44ec74d52a6283349f362105a4dac8a941ed10
Mellified <81110968+Mellified@users.noreply.github.com>
2021-03-29 18:32:44 -0500

    Updated to generate its own MRA list

--- FeelLucky_Arcade.sh
+++ FeelLucky_Arcade.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-# This loads a random arcade core from Attract_Arcade.txt
+# This loads a random arcade core from all MRAs
 #
 # https://github.com/mrchrisster/mister-arcade-attract/
 
 
+# Dump a list of all MRAs
+mralist="/tmp/Attract_Arcade.txt"
+ls -N1 /media/fat/_Arcade/*.mra | sed 's/\/media\/fat\/_Arcade\///' > ${mralist}
+
+# Greeting
+clear
 echo "So you're feeling lucky?"
 echo ""
+
 # Get a random game from the list
-mra=$(shuf -n 1 /media/fat/Scripts/Attract_Arcade.txt)
+mra=$(shuf -n 1 ${mralist})
 # If the mra variable is valid this should immediately pass, but if not it'll keep trying
 # Partially protects against typos from manual editing and strange character parsing problems
 until [ -f "/media/fat/_Arcade/${mra}" ]; do
-	mra=$(shuf -n 1 /media/fat/Scripts/Attract_Arcade.txt)
+	mra=$(shuf -n 1 ${mralist})
 done
-# Strip the extension
+
+# Strip the extension for display purposes
 mra_short=$(echo "${mra}" | sed -e 's/\.[^.]*$//')
 
 echo "You'll be playing:"
