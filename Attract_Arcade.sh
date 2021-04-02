6678cafe8045c29b26f6353fcdf6be3306405d78
Mellified <81110968+Mellified@users.noreply.github.com>
2021-04-02 01:08:41 -0500

    Fix for empty exclude list, debug added

--- Attract_Arcade.sh
+++ Attract_Arcade.sh
@@ -11,34 +11,62 @@
 # And thanks to kaloun34 for contributing!
 # https://github.com/mrchrisster/mister-arcade-attract/
 
-#Default values
-mrapath="/media/fat/_Arcade"
-
-# Time before going to the next core
+#Default values - Recommend changing values in the INI file, not here.
+mrapath="/media/fat/_Arcade/"
+mrapathvert="/media/fat/_Arcade/_Organized/_6 Rotation/_Vertical CW 90 Deg/"
+mrapathhoriz="/media/fat/_Arcade/_Organized/_6 Rotation/_Horizontal/"
+declare -a mraexclude
 timer=120
-
-# The following two Directories are for your horizontal and vertical games - requires Arcade Organizer Script
-# You can also set the vertical Games to "/media/fat/_Arcade/_Organized/_6 Rotation/_Vertical CCW 90 Deg" for Games that are rotated counter clockwise
-mrapathvert="/media/fat/_Arcade/_Organized/_6 Rotation/_Vertical CW 90 Deg" 
-mrapathhoriz="/media/fat/_Arcade/_Organized/_6 Rotation/_Horizontal"
-
-# Which games do you want to display? - Set it to Horizontal, Vertical or All
 orientation=All
-
-# List of MRAs. If no list is found, the script will use a temporary list
 mralist="/media/fat/Scripts/Attract_Arcade.txt"
+debugmain=false
+debugfunc=false
+tracemain=false
+tracefunc=false
+traceparse_ini=false
+tracebuild_mralist=false
+tracenext_core=false
+
 
 ## Functions
 
 parse_ini()
 {
+	if ${debugfunc}; then echo "Inside parse_ini"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 	# INI Parsing
-	. /media/fat/Scripts/Attract_Arcade.ini
-	IFS=$'\n'
+	basepath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+	if [ -f ${basepath}/Attract_Arcade.ini ]; then
+		. ${basepath}/Attract_Arcade.ini
+		IFS=$'\n'
+	fi
+
+	# Remove trailing slash from paths
+	for var in mrapath mrapathvert mrapathhoriz; do
+		if ${traceparse_ini}; then echo "var=${var} | \${!var}=${!var} | \${!var%/}=${!var%/}"; fi
+		declare -g ${var}="${!var%/}"
+		if ${traceparse_ini}; then echo "${var}=${!var}"; fi
+	done
+
+	# Set mrapath based on orientation
+	if ${traceparse_ini}; then echo "mrapath=${mrapath}"; fi
+	if [ "${orientation}" == "Vertical" ]; then
+		mrapath="${mrapathvert}"
+	elif [ "${orientation}" == "Horizontal" ]; then
+		mrapath="${mrapathhoriz}"
+	fi
+	if ${traceparse_ini}; then echo "mrapath=${mrapath}"; fi
+	
+	if ${debugfunc}; then echo "Leaving parse_ini"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 parse_cmdline()
 {
+	if ${debugfunc}; then echo "Entered parse_cmdline"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 	# Load the next core and exit - for testing via ssh
 	# Won't reset the timer!
 	case "${1}" in
@@ -47,10 +75,16 @@ parse_cmdline()
 					exit 0
 					;;
 	esac
+	
+	if ${debugfunc}; then echo "Leaving parse_cmdline"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 there_can_be_only_one()
 {
+	if ${debugfunc}; then echo "Entered there_can_be_only_one"; fi
+	if ${tracefunc}; then	echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 	# If another attract process is running kill it
 	# This can happen if the script is started multiple times
 	if [ -f /var/run/attract.pid ]; then
@@ -58,88 +92,74 @@ there_can_be_only_one()
 	fi
 	# Save our PID
 	echo "$(pidof $(basename ${1}))" > /var/run/attract.pid
+	
+	if ${debugfunc}; then echo "Leaving there_can_be_only_one"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 build_mralist()
 {
-	# If the file does not exist make one in /tmp/
-	if [ $orientation = "All" ]; then
+	if ${debugfunc}; then echo "Entered build_mralist"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 
-		if [ ! -f ${mralist} ]; then
-			mralist="/tmp/Attract_Arcade.txt"
-		
-			# If no MRAs found - suicide!
-			find "${mrapath}" -maxdepth 1 -type f \( -iname "*.mra" \) &>/dev/null
-			if [ ! ${?} == 0 ]; then
-				echo "The path ${mrapath} contains no MRA files!"
-				exit 1
-			fi
-		
-			# This prints the list of MRA files in a path,
-			# Cuts the string to just the file name,
-			# Then saves it to the mralist file.
-			find "${mrapath}" -maxdepth 1 -type f \( -iname "*.mra" \) | cut -c $(( $(echo ${#mrapath}) + 2 ))- | grep -vFf <(printf '%s\n' ${mraexclude[@]}) > ${mralist}
-		fi
-	elif [ $orientation = "Horizontal" ]; then
-		
-		if [ ! -f ${mralist} ]; then
-			mralist="/tmp/Attract_Arcade.txt"
-		
-			# If no MRAs found - suicide!
-			find "${mrapathhoriz}" -maxdepth 1 -type f \( -iname "*.mra" \) &>/dev/null
-			if [ ! ${?} == 0 ]; then
-				echo "The path ${mrapathhoriz} contains no MRA files!"
-				exit 1
-			fi
-		
-			# This prints the list of MRA files in a path,
-			# Cuts the string to just the file name,
-			# Then saves it to the mralist file.
-			find "${mrapathhoriz}" -maxdepth 1 -type f \( -iname "*.mra" \) | cut -c $(( $(echo ${#mrapath}) + 2 ))- | grep -vFf <(printf '%s\n' ${mraexclude[@]}) > ${mralist}
-		fi
-	elif [ $orientation = "Vertical" ]; then
-		
-		if [ ! -f ${mralist} ]; then
-			mralist="/tmp/Attract_Arcade.txt"
-		
-			# If no MRAs found - suicide!
-			find "${mrapathvert}" -maxdepth 1 -type f \( -iname "*.mra" \) &>/dev/null
-			if [ ! ${?} == 0 ]; then
-				echo "The path ${mrapathvert} contains no MRA files!"
-				exit 1
-			fi
-		
-			# This prints the list of MRA files in a path,
-			# Cuts the string to just the file name,
-			# Then saves it to the mralist file.
-			find "${mrapathvert}" -maxdepth 1 -type f \( -iname "*.mra" \) | cut -c $(( $(echo ${#mrapath}) + 2 ))- | grep -vFf <(printf '%s\n' ${mraexclude[@]}) > ${mralist}
-		fi
+	# If the file does not exist make one in /tmp/
+	if [ ! -f ${mralist} ]; then
+		mralist="/tmp/Attract_Arcade.txt"
 	fi
+	
+	# If no MRAs found - suicide!
+	find "${mrapath}" -maxdepth 1 -type f \( -iname "*.mra" \) &>/dev/null
+	if [ ! ${?} == 0 ]; then
+		echo "The path ${mrapath} contains no MRA files!"
+		exit 1
+	fi
+	
+	# This prints the list of MRA files in a path,
+	# Cuts the string to just the file name,
+	# Then saves it to the mralist file.
+	
+	# If there is an empty exclude list ignore it
+	# Otherwise use it to filter the list
+	if [ ${#mraexclude[@]} -eq 0 ]; then
+		find "${mrapath}" -maxdepth 1 -type f \( -iname "*.mra" \) | cut -c $(( $(echo ${#mrapath}) + 2 ))- >"${mralist}"
+	else
+		find "${mrapath}" -maxdepth 1 -type f \( -iname "*.mra" \) | cut -c $(( $(echo ${#mrapath}) + 2 ))- | grep -vFf <(printf '%s\n' ${mraexclude[@]})>"${mralist}"
+	fi
+	
+	if ${debugfunc}; then echo "Leaving build_mralist"; fi
+	if ${tracefunc}; then	echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 next_core()
 {
+	if ${debugfunc}; then echo "Entered next_core"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+	if ${tracenext_core}; then echo "Seeding \$mra from mralist=${mralist}"; fi
+
 	# Get a random game from the list
-	mra=$(shuf -n 1 ${mralist})
+	mra="$(shuf -n 1 ${mralist})"
+
+	if ${tracenext_core}; then echo "mra=${mra}"; fi
 
 	# If the mra variable is valid this is skipped, but if not it'll try 10 times
 	# Partially protects against typos from manual editing and strange character parsing problems
 	for i in {1..10}; do
 		if [ ! -f "${mrapath}/${mra}" ]; then
 			mra=$(shuf -n 1 ${mralist})
+			if ${tracenext_core}; then echo "mra=${mra}"; fi
 		fi
 	done
 	# If the MRA is still not valid something is wrong - suicide
 	if [ ! -f "${mrapath}/${mra}" ]; then
+		echo "There is no valid file at ${mrapath}/${mra}!"
 		exit 1
 	fi
 
 	echo "You'll be playing:"
 	# Bold the MRA name - remove trailing .mra
-	echo -e "\e[1m $(echo $(basename "${mra}") | sed -e 's/\.[^.]*$//')"
-	# Reset text
-	echo -e "\e[0m"
+	echo -e "\e[1m $(echo $(basename "${mra}") | sed -e 's/\.[^.]*$//') \e[0m"
 
+	if ${tracenext_core}; then echo "cmdline=${1}"; fi
 	if [ "${1}" == "quarters" ]; then
 		echo "Loading quarters in..."
 		for i in {5..1}; do
@@ -148,34 +168,74 @@ next_core()
 		done
 	fi
 
+	if ${tracenext_core}; then echo "command is load_core ${mrapath}/${mra}"; fi
   # Tell MiSTer to load the next MRA
   echo "load_core ${mrapath}/${mra}" > /dev/MiSTer_cmd
+  
+  if ${debugfunc}; then echo "Leaving next_core"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 loop_core()
 {
+	if ${debugfunc}; then echo "Entered loop_core"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 	while :; do
 		next_core
   	sleep ${timer}
 	done
+	
+	if ${debugfunc}; then echo "Leaving loop_core"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 get_lucky()
 {
+	if ${debugfunc}; then echo "Entered get_lucky"; fi
+	if ${tracefunc}; then	echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 	echo "So you're feeling lucky?"
 	echo ""
 	
 	next_core quarters
+	
+	if ${debugfunc}; then echo "Leaving get_lucky"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 
 ## Prep
+if ${debugmain}; then echo -e "\e[1mEntering parse_ini\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 parse_ini
+if ${debugmain}; then echo -e "\e[1mLeft parse_ini\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
+if ${debugmain}; then	echo -e "\e[1mEntering build_mralist\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 build_mralist
+if ${debugmain}; then echo -e "\e[1mLeft build_mralist\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
+if ${debugmain}; then	echo -e "\e[1mEntering parse_cmdline\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 parse_cmdline ${1}
+if ${debugmain}; then echo -e "\e[1mLeft parse_cmdline\e[0m"; fi
+if ${tracemain}; then	echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
+if ${debugmain}; then echo -e "\e[1mEntering there_can_be_only_one\e[0m"; fi
+if ${tracemain}; then	echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 there_can_be_only_one ${0}
+if ${debugmain}; then echo -e "\e[1mLeft there_can_be_only_one\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 
 
 ## Main
+if ${debugmain}; then echo -e "\e[1mEntering loop_core\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 loop_core
+if ${debugmain}; then echo -e "\e[1mLeft loop_core\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 exit 0
--- FeelLucky_Arcade.sh
+++ FeelLucky_Arcade.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ## Description
-# This loads a random arcade core from all MRAs
-# Then displays the result before loading it
+# This cycles through arcade cores periodically
+# Games are randomly pulled from all MRAs or a user-provided list
 
 
 ## Credits
@@ -11,30 +11,62 @@
 # And thanks to kaloun34 for contributing!
 # https://github.com/mrchrisster/mister-arcade-attract/
 
-
-## Variables
-# Edit here or save preferred values in
-# /media/fat/Scripts/Arcade_Attract.ini
-# Example:
-# mrapath=/media/usb0/_Arcade
-
-# Vertical files to use
-mravert="_Vertical CW 90 Deg"
-# Directory for _Arcade files - no trailing slash!
-mrapath="/media/fat/_Arcade"
-#mrapath="/media/fat/_Arcade/_Organized/_6 Rotation/_Horizontal"
-#mrapath="/media/fat/_Arcade/_Organized/_6 Rotation/${mravert}"
-# Time before going to the next core
+#Default values - Recommend changing values in the INI file, not here.
+mrapath="/media/fat/_Arcade/"
+mrapathvert="/media/fat/_Arcade/_Organized/_6 Rotation/_Vertical CW 90 Deg/"
+mrapathhoriz="/media/fat/_Arcade/_Organized/_6 Rotation/_Horizontal/"
+declare -a mraexclude
 timer=120
-# List of MRAs
+orientation=All
 mralist="/media/fat/Scripts/Attract_Arcade.txt"
-# Excluded MRAs
-declare -a mraexclude=('Example Bad.mra' 'Fake Example.mra')
+debugmain=false
+debugfunc=false
+tracemain=false
+tracefunc=false
+traceparse_ini=false
+tracebuild_mralist=false
+tracenext_core=false
 
 
 ## Functions
+
+parse_ini()
+{
+	if ${debugfunc}; then echo "Inside parse_ini"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
+	# INI Parsing
+	basepath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+	if [ -f ${basepath}/Attract_Arcade.ini ]; then
+		. ${basepath}/Attract_Arcade.ini
+		IFS=$'\n'
+	fi
+
+	# Remove trailing slash from paths
+	for var in mrapath mrapathvert mrapathhoriz; do
+		if ${traceparse_ini}; then echo "var=${var} | \${!var}=${!var} | \${!var%/}=${!var%/}"; fi
+		declare -g ${var}="${!var%/}"
+		if ${traceparse_ini}; then echo "${var}=${!var}"; fi
+	done
+
+	# Set mrapath based on orientation
+	if ${traceparse_ini}; then echo "mrapath=${mrapath}"; fi
+	if [ "${orientation}" == "Vertical" ]; then
+		mrapath="${mrapathvert}"
+	elif [ "${orientation}" == "Horizontal" ]; then
+		mrapath="${mrapathhoriz}"
+	fi
+	if ${traceparse_ini}; then echo "mrapath=${mrapath}"; fi
+	
+	if ${debugfunc}; then echo "Leaving parse_ini"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+}
+
 parse_cmdline()
 {
+	if ${debugfunc}; then echo "Entered parse_cmdline"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 	# Load the next core and exit - for testing via ssh
 	# Won't reset the timer!
 	case "${1}" in
@@ -43,10 +75,16 @@ parse_cmdline()
 					exit 0
 					;;
 	esac
+	
+	if ${debugfunc}; then echo "Leaving parse_cmdline"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 there_can_be_only_one()
 {
+	if ${debugfunc}; then echo "Entered there_can_be_only_one"; fi
+	if ${tracefunc}; then	echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 	# If another attract process is running kill it
 	# This can happen if the script is started multiple times
 	if [ -f /var/run/attract.pid ]; then
@@ -54,63 +92,74 @@ there_can_be_only_one()
 	fi
 	# Save our PID
 	echo "$(pidof $(basename ${1}))" > /var/run/attract.pid
-}
-
-parse_ini()
-{
-	# INI Parsing - be kind
-	if [ -f /media/fat/Scripts/Attract_Arcade.ini ]; then
-		while IFS='= ' read var val; do
-			if [[ ${val} ]]; then
-	      declare -g "${var}=${val}"
-	    fi
-		done < /media/fat/Scripts/Attract_Arcade.ini
-	fi
+	
+	if ${debugfunc}; then echo "Leaving there_can_be_only_one"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 build_mralist()
 {
+	if ${debugfunc}; then echo "Entered build_mralist"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 	# If the file does not exist make one in /tmp/
 	if [ ! -f ${mralist} ]; then
 		mralist="/tmp/Attract_Arcade.txt"
-		
-		# If no MRAs found - suicide!
-		find "${mrapath}" -maxdepth 1 -type f \( -iname "*.mra" \) &>/dev/null
-		if [ ! ${?} == 0 ]; then
-			echo "The path ${mrapath} contains no MRA files!"
-			exit 1
-		fi
-		
-		# This prints the list of MRA files in a path,
-		# Cuts the string to just the file name,
-		# Then saves it to the mralist file.
-		find "${mrapath}" -maxdepth 1 -type f \( -iname "*.mra" \) | cut -c $(( $(echo ${#mrapath}) + 2 ))- | grep -vFf <(printf '%s\n' ${mraexclude[@]}) > ${mralist}
 	fi
+	
+	# If no MRAs found - suicide!
+	find "${mrapath}" -maxdepth 1 -type f \( -iname "*.mra" \) &>/dev/null
+	if [ ! ${?} == 0 ]; then
+		echo "The path ${mrapath} contains no MRA files!"
+		exit 1
+	fi
+	
+	# This prints the list of MRA files in a path,
+	# Cuts the string to just the file name,
+	# Then saves it to the mralist file.
+	
+	# If there is an empty exclude list ignore it
+	# Otherwise use it to filter the list
+	if [ ${#mraexclude[@]} -eq 0 ]; then
+		find "${mrapath}" -maxdepth 1 -type f \( -iname "*.mra" \) | cut -c $(( $(echo ${#mrapath}) + 2 ))- >"${mralist}"
+	else
+		find "${mrapath}" -maxdepth 1 -type f \( -iname "*.mra" \) | cut -c $(( $(echo ${#mrapath}) + 2 ))- | grep -vFf <(printf '%s\n' ${mraexclude[@]})>"${mralist}"
+	fi
+	
+	if ${debugfunc}; then echo "Leaving build_mralist"; fi
+	if ${tracefunc}; then	echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 next_core()
 {
+	if ${debugfunc}; then echo "Entered next_core"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+	if ${tracenext_core}; then echo "Seeding \$mra from mralist=${mralist}"; fi
+
 	# Get a random game from the list
-	mra=$(shuf -n 1 ${mralist})
+	mra="$(shuf -n 1 ${mralist})"
+
+	if ${tracenext_core}; then echo "mra=${mra}"; fi
 
 	# If the mra variable is valid this is skipped, but if not it'll try 10 times
 	# Partially protects against typos from manual editing and strange character parsing problems
 	for i in {1..10}; do
 		if [ ! -f "${mrapath}/${mra}" ]; then
 			mra=$(shuf -n 1 ${mralist})
+			if ${tracenext_core}; then echo "mra=${mra}"; fi
 		fi
 	done
 	# If the MRA is still not valid something is wrong - suicide
 	if [ ! -f "${mrapath}/${mra}" ]; then
+		echo "There is no valid file at ${mrapath}/${mra}!"
 		exit 1
 	fi
 
 	echo "You'll be playing:"
 	# Bold the MRA name - remove trailing .mra
-	echo -e "\e[1m $(echo $(basename "${mra}") | sed -e 's/\.[^.]*$//')"
-	# Reset text
-	echo -e "\e[0m"
+	echo -e "\e[1m $(echo $(basename "${mra}") | sed -e 's/\.[^.]*$//') \e[0m"
 
+	if ${tracenext_core}; then echo "cmdline=${1}"; fi
 	if [ "${1}" == "quarters" ]; then
 		echo "Loading quarters in..."
 		for i in {5..1}; do
@@ -119,34 +168,74 @@ next_core()
 		done
 	fi
 
+	if ${tracenext_core}; then echo "command is load_core ${mrapath}/${mra}"; fi
   # Tell MiSTer to load the next MRA
   echo "load_core ${mrapath}/${mra}" > /dev/MiSTer_cmd
+  
+  if ${debugfunc}; then echo "Leaving next_core"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 loop_core()
 {
+	if ${debugfunc}; then echo "Entered loop_core"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 	while :; do
 		next_core
   	sleep ${timer}
 	done
+	
+	if ${debugfunc}; then echo "Leaving loop_core"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 get_lucky()
 {
+	if ${debugfunc}; then echo "Entered get_lucky"; fi
+	if ${tracefunc}; then	echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 	echo "So you're feeling lucky?"
 	echo ""
 	
 	next_core quarters
+	
+	if ${debugfunc}; then echo "Leaving get_lucky"; fi
+	if ${tracefunc}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 }
 
 
 ## Prep
+if ${debugmain}; then echo -e "\e[1mEntering parse_ini\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 parse_ini
+if ${debugmain}; then echo -e "\e[1mLeft parse_ini\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
+if ${debugmain}; then	echo -e "\e[1mEntering build_mralist\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 build_mralist
+if ${debugmain}; then echo -e "\e[1mLeft build_mralist\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
+if ${debugmain}; then	echo -e "\e[1mEntering parse_cmdline\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 parse_cmdline ${1}
+if ${debugmain}; then echo -e "\e[1mLeft parse_cmdline\e[0m"; fi
+if ${tracemain}; then	echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
+if ${debugmain}; then echo -e "\e[1mEntering there_can_be_only_one\e[0m"; fi
+if ${tracemain}; then	echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 there_can_be_only_one ${0}
+if ${debugmain}; then echo -e "\e[1mLeft there_can_be_only_one\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 
 
 ## Main
+if ${debugmain}; then echo -e "\e[1mEntering get_lucky\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
 get_lucky
+if ${debugmain}; then echo -e "\e[1mLeft get_lucky\e[0m"; fi
+if ${tracemain}; then echo "mrapath=${mrapath} | mrapathvert=${mrapathvert} | mrapathhoriz=${mrapathhoriz} | mraexclude=${mrapathexclude} | timer=${timer} | orientation=${orientation} | mralist=${mralist}"; fi
+
 exit 0
